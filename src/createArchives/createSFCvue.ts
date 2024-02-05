ed07b105f20fb47a9cbf49288c98b1623a498ac8
gustavorteuber <gugareis135@gmail.com>
2024-02-05 16:27:55 -0300

    1.8.5 lint

--- src/createArchives/createSFCvue.ts
+++ src/createArchives/createSFCvue.ts
@@ -3,11 +3,11 @@ import * as fs from 'fs'
 
 export function createVueFile(moduleName, nameArchive) {
   const vueContent = `
-  <template>
-    <div>
-      <h1>Rafa</h1>
-    </div>
-  </template>
+<template>
+  <div>
+    <h1>Rafa</h1>
+  </div>
+</template>
   `
   const vueWorkspace = `./src/views/${moduleName}/${nameArchive}.vue`
   fs.writeFileSync(vueWorkspace, vueContent)
--- src/createArchives/createState.ts
+++ src/createArchives/createState.ts
@@ -3,25 +3,25 @@ import * as fs from 'fs'
 
 export function createStoreFile(moduleName: string, nameArchive: string) {
   const tsContent = `
-  import { defineStore } from 'pinia'
-  import axios from '@axios'
-  
-  export const useRafaStore = defineStore('${moduleName}', {
-    state: () => ({
-    }),
-    actions: {
-      async firstAction() {
-        try {
-          const response = await axios.get('/')
-  
-          return response.data
-        }
-        catch (error: any) {
-          return error.response.data.message
-        }
-      },
+import { defineStore } from 'pinia'
+import axios from '@axios'
+
+export const useRafaStore = defineStore('${moduleName}', {
+  state: () => ({
+  }),
+  actions: {
+    async firstAction() {
+      try {
+        const response = await axios.get('/')
+
+        return response.data
+      }
+      catch (error: any) {
+        return error.response.data.message
+      }
     },
-  })
+  },
+})
   
   `
   const tsWorkspace = `./src/stores/${moduleName}/${nameArchive}.ts`
