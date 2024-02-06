f706e2e61e2599384390d2a402d4da385895cc76
gustavorteuber <gugareis135@gmail.com>
2024-02-06 16:23:14 -0300

    colors :)

--- src/art/ascii.ts
+++ src/art/ascii.ts
@@ -1,24 +1,36 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-const asciiArt = `                                        
-                   :=:                  
-                :+%#*##+:               
-                -+++*++++.              
-          -+: :=+*******+-. .+-         
-       -*%+-:+*************=.-*%*-      
-      *%%+::=--=********+---+ :*%%+     
-      *%:=#%***=--=**+---+***#*--%+     
-      *%.  :******=-.-+******   :%+     
-      *%.  :****#%#*:##%#****   :%+     
-      *%.   :=****#%%#*****=:   :%+     Seja bem-vindo ao PrismaBox-CLI!
-      *%.      :+**#%#**=:      :%+            by: gustavorteuber
-      *%-        .-*%+:.        =%+     
-      .=##+:  .....+%-.......-+%*-      
-         .=#%*-....+%-....=*%*=.        
-         ...:=*%*-.+%-:=*%*=...         
-            ....=*%#%#%*-...            
-                ...-+-...               
-                   ...                  
-`
+
+const colors = {
+  reset: '\x1b[0m',
+  red: '\x1b[31m',
+  blue: '\x1b[32m',
+  yellow: '\x1b[33m',
+  magenta: '\x1b[35m',
+  cyan: '\x1b[36m',
+  gray: '\x1b[90m',
+}
+
+const asciiArt = `
+${colors.cyan}                                        
+${colors.cyan}                   :=:                  
+${colors.cyan}                :+%#*##+:               
+${colors.cyan}                -+++*++++.              
+${colors.cyan}          -+: :=+*******+-. .+-         
+${colors.cyan}       -*%+-:+*************=.-*%*-      
+${colors.cyan}      *%%+::=--=********+---+ :*%%+     
+${colors.cyan}      *%:=#%***=--=**+---+***#*--%+     
+${colors.cyan}      *%.  :******=-.-+******   :%+     
+${colors.cyan}      *%.  :****#%#*:##%#****   :%+     
+${colors.cyan}      *%.   :=****#%%#*****=:   :%+     ${colors.red}Seja bem-vindo ao PrismaBox-CLI!
+${colors.cyan}      *%.      :+**#%#**=:      :%+            ${colors.gray}by: gustavorteuber
+${colors.cyan}      *%-        .-*%+:.        =%+     
+${colors.cyan}      .=##+:  .....+%-.......-+%*-      
+${colors.cyan}         .=#%*-....+%-....=*%*=.        
+${colors.cyan}            ....=*%#%#%*-...            
+${colors.cyan}                ...-+-...               
+${colors.cyan}                   ...                  
+${colors.reset}`
+
 function resizeAscii(ascii: string, width: number) {
   const lines = ascii.split('\n')
   const resizedAscii = lines
